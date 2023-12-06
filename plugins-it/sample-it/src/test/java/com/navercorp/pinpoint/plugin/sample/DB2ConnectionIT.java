a5ce7ab85339dfaddedaeb6ad4ce9310e721819f
BEADYbirds <112479040+BEADYbirds@users.noreply.github.com>
2023-12-06 17:49:00 +0900

    ㅎnn

--- plugins-it/sample-it/src/test/java/com/navercorp/pinpoint/plugin/sample/DB2ConnectionIT.java
+++ plugins-it/sample-it/src/test/java/com/navercorp/pinpoint/plugin/sample/DB2ConnectionIT.java
@@ -43,8 +43,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
+
 
 /**
  * @author Woonduk Kang(emeroad)
